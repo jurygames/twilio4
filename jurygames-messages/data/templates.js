014fd98be18870071ee9a7c794532fee31421b9a
jurygames <info@jurygames.com>
2025-07-06 14:12:51 +0100

    Update templates.js

--- jurygames-messages/data/templates.js
+++ jurygames-messages/data/templates.js
@@ -1,7 +1,5 @@
-
 // data/templates.js
 const templates = [
-  // SMS Templates
   {
     name: "Info – Scott Davies",
     type: "SMS",
@@ -16,7 +14,132 @@ const templates = [
     from: "+18448997279",
     content: "I’m so sorry to reach out like this, but I have information about the Scott Davies case. Please email me at knockknock502@gmail.com\nIt’s about Reggie and Mel"
   },
-  // ... include all other templates here ...
+  {
+    name: "Help Link",
+    type: "SMS",
+    show: "Scott Davies",
+    from: "+447723453049",
+    content: "I’m still trying to help you and your colleagues where I can. You may find something very useful here: https://www.ukcellnet.co.uk/\nSee anyone familiar?"
+  },
+  {
+    name: "Help Link (US+)",
+    type: "SMS",
+    show: "Scott Davies",
+    from: "+18448997279",
+    content: "I’m still trying to help you and your colleagues where I can. You may find something very useful here: https://www.ukcellnet.co.uk/\nSee anyone familiar?"
+  },
+  {
+    name: "Hook Message",
+    type: "SMS",
+    show: "Harry Briggs",
+    from: "+447723453049",
+    content: "They've got you on a hook, little fish. You're not seeing the big picture.\nRichard Holmes wasn't a cleaner.\nDon't take my word for it. Look him up on their own database.\nHis name was Richard Holmes. Look him up.\n\nStop being the fish. Start being the hook."
+  },
+  {
+    name: "Hook Message (US+)",
+    type: "SMS",
+    show: "Harry Briggs",
+    from: "+18448997279",
+    content: "They've got you on a hook, little fish. You're not seeing the big picture.\nRichard Holmes wasn't a cleaner.\nDon't take my word for it. Look him up on their own database.\nHis name was Richard Holmes. Look him up.\n\nStop being the fish. Start being the hook."
+  },
+  {
+    name: "Favour for Chris",
+    type: "SMS",
+    show: "Scott Davies",
+    from: "+447723453049",
+    content: "Tell Chris this is the last favour like this I do... Interpol isn't a personal Google! https://tinyurl.com/y57vk6c7"
+  },
+  {
+    name: "Favour for Chris (US+)",
+    type: "SMS",
+    show: "Scott Davies",
+    from: "+18448997279",
+    content: "Tell Chris this is the last favour like this I do... Interpol isn't a personal Google! https://tinyurl.com/y57vk6c7"
+  },
+  {
+    name: "PC Evans – Sian IG",
+    type: "SMS",
+    show: "Christmas Party",
+    from: "+447723453049",
+    content: "This is PC Evans. Sian Jones texted me this Instagram post, you can see it for yourself here: tinyurl.com/sianjonesinstagram"
+  },
+  {
+    name: "PC Evans – Sian IG (US+)",
+    type: "SMS",
+    show: "Christmas Party",
+    from: "+18448997279",
+    content: "This is PC Evans. Sian Jones texted me this Instagram post, you can see it for yourself here: tinyurl.com/sianjonesinstagram"
+  },
+  {
+    name: "Harry's Blog",
+    type: "SMS",
+    show: "Harry Briggs",
+    from: "+447480780992",
+    content: "There’s a lot you don’t know about Harry.\n\nI wonder what he’d say if he knew you’d read his blog?\n\nhttps://incendiary-topics.blogspot.com/"
+  },
+  {
+    name: "Harry's Blog (US+)",
+    type: "SMS",
+    show: "Harry Briggs",
+    from: "+18448997279",
+    content: "There’s a lot you don’t know about Harry.\n\nI wonder what he’d say if he knew you’d read his blog?\n\nhttps://incendiary-topics.blogspot.com/"
+  },
+  {
+    name: "Desk Blocking",
+    type: "SMS",
+    show: "Harry Briggs",
+    from: "+447480780992",
+    content: "A man died. Don't forget that.\n\nHow did the desk end up blocking the doorway?"
+  },
+  {
+    name: "Desk Blocking (US+)",
+    type: "SMS",
+    show: "Harry Briggs",
+    from: "+18448997279",
+    content: "A man died. Don't forget that.\n\nHow did the desk end up blocking the doorway?"
+  },
+  {
+    name: "TI Sally Call",
+    type: "Call",
+    show: "Scott Davies",
+    from: "+447723453049",
+    mediaUrl: "https://img1.wsimg.com/blobby/go/a1fcfcf6-ebb3-42af-900f-bac6da508cb5/downloads/2649e7ca-7852-40f1-b3f1-fd2a9094f0d1/ti-sally-call.mp3?ver=1751047203736"
+  },
+  {
+    name: "TI Sally Call (US+)",
+    type: "Call",
+    show: "Scott Davies",
+    from: "+18448997279",
+    mediaUrl: "https://img1.wsimg.com/blobby/go/a1fcfcf6-ebb3-42af-900f-bac6da508cb5/downloads/2649e7ca-7852-40f1-b3f1-fd2a9094f0d1/ti-sally-call.mp3?ver=1751047203736"
+  },
+  {
+    name: "JD Call 1",
+    type: "Call",
+    show: "Harry Briggs",
+    from: "+447723453049",
+    mediaUrl: "https://img1.wsimg.com/blobby/go/a1fcfcf6-ebb3-42af-900f-bac6da508cb5/downloads/624ab114-69cc-4556-842e-e1485786b325/jd-call-1.mp3?ver=1751047203736"
+  },
+  {
+    name: "JD Call 1 (US+)",
+    type: "Call",
+    show: "Harry Briggs",
+    from: "+18448997279",
+    mediaUrl: "https://img1.wsimg.com/blobby/go/a1fcfcf6-ebb3-42af-900f-bac6da508cb5/downloads/624ab114-69cc-4556-842e-e1485786b325/jd-call-1.mp3?ver=1751047203736"
+  },
+  {
+    name: "JD Call 2",
+    type: "Call",
+    show: "Harry Briggs",
+    from: "+447480780992",
+    mediaUrl: "https://img1.wsimg.com/blobby/go/a1fcfcf6-ebb3-42af-900f-bac6da508cb5/downloads/9dd85999-425a-4546-adad-62ca92194f00/jd-call-2.mp3?ver=1751047203736"
+  },
+  {
+    name: "JD Call 2 (US+)",
+    type: "Call",
+    show: "Harry Briggs",
+    from: "+18448997279",
+    mediaUrl: "https://img1.wsimg.com/blobby/go/a1fcfcf6-ebb3-42af-900f-bac6da508cb5/downloads/9dd85999-425a-4546-adad-62ca92194f00/jd-call-2.mp3?ver=1751047203736"
+  }
 ];
 
 export default templates;
