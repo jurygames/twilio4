0719033b5ff8dc8e95f438740d350a29f26b385b
jurygames <info@jurygames.com>
2025-07-06 15:11:16 +0100

    Update index.js

--- jurygames-messages/pages/index.js
+++ jurygames-messages/pages/index.js
@@ -1,5 +1,7 @@
+
 // pages/index.js
 import Head from 'next/head';
+import Link from 'next/link';
 import GroupManager from '../components/GroupManager';
 import SendPanel from '../components/SendPanel';
 import StatusPanel from '../components/StatusPanel';
@@ -16,8 +18,8 @@ export default function Home() {
       </Head>
       <div className="dark bg-gray-900 text-white min-h-screen">
         <header className="bg-[#333F50] py-6 flex items-center justify-center">
-          <h1 style={{ fontFamily: "'Roboto Condensed', sans-serif", fontWeight: 700 }} className="text-3xl uppercase text-white">
-            JURY GAMES
+          <h1 className="text-3xl font-bold font-['Roboto_Condensed'] uppercase text-white">
+            Jury Games
           </h1>
         </header>
         <main className="p-6 grid md:grid-cols-2 gap-6">
@@ -29,6 +31,14 @@ export default function Home() {
             <StatusPanel logs={logs} />
           </div>
         </main>
+        <div className="p-6 flex justify-center">
+          <Link href="/admin">
+            <button className="bg-gray-800 hover:bg-gray-700 text-white px-4 py-2 rounded inline-flex items-center">
+              <span className="mr-2">🔧</span>
+              Manage Templates
+            </button>
+          </Link>
+        </div>
       </div>
     </>
   );
