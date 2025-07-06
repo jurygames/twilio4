be801579ea919bf84f763e9542d7732a252d8fb8
jurygames <info@jurygames.com>
2025-07-06 13:54:17 +0100

    Update _app.js

--- jurygames-messages/pages/_app.js
+++ jurygames-messages/pages/_app.js
@@ -1,1 +1,16 @@
-< _app.js content>
+// pages/_app.js
+import '../styles/globals.css';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
